52dbb392746047488034f58aa28f8b8a7d533f9d
niranjan <niranjan.upari@gmail.com>
2026-03-16 21:59:24 +0530

    Taking multiple matching locator elements in an array

--- tests/UiBasics.spec.js
+++ tests/UiBasics.spec.js
@@ -17,11 +17,26 @@ test.only('traditional way using browser, context, page', async({browser})=>{
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
     console.log(await page.title());
     //css selectors
-    await page.locator('input#username').fill("3rahulshettyacademy")
-    await page.locator('[type="password"]').fill("Learning@830$3mK2")
-    await page.locator('[name="signin"]').click()
+
+    // mentioning locators here with names
+    const userName = page.locator('input#username')
+    const password = page.locator('[type="password"]')
+    const signIn = page.locator('[name="signin"]')
+    const cardTitles = page.locator(".card-body a")
+    await userName.fill("3rahulshettyacademy")
+    await password.fill("Learning@830$3mK2")
+    await signIn.click()
+
     console.log(await page.locator('[style*="block"]').textContent())
     await expect(page.locator('[style*="block"]')).toContainText('Incorrect')
 
+    await userName.fill("") // It erases the existing text with blank
+    await userName.fill("rahulshettyacademy")
+    await signIn.click()
+
+    console.log(await cardTitles.nth(1).textContent()) //when locator is returning multiple things, lets use nth/first/last
+    console.log(await cardTitles.first().textContent())
+    const allTitles = await cardTitles.allTextContents()
+    console.log(allTitles)
 });
 
