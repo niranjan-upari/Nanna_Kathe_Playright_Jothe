6225ad5d30e79de578a92c0abf02b010a3efd3e9
niranjan <niranjan.upari@gmail.com>
2026-05-09 19:50:24 +0530

    Rewriting assignment1 using PLAYWRIGHT special locators- part 2

--- tests/assignment1_Rewrite.spec.js
+++ tests/assignment1_Rewrite.spec.js
@@ -21,4 +21,18 @@ test('Assignment 1 -Rewriting using Playwright special locators - test', async({
     await page.locator(".card-body").filter({hasText:productName}).getByRole("button", {name:"Add To Cart"}).click() // we can also use filter method to find the product and click on add to cart button
 
     await page.getByRole("listitem").getByRole("button", {name:"Cart"}).click() // click on cart button
+
+
+    await page.locator("div li").first().waitFor() //wait for cart page to load
+
+    await expect(page.getByText("ZARA COAT 3")).toBeVisible() // verify the product is added to cart
+
+    await page.getByText("Checkout").click() // click on checkout button
+
+    await page.getByPlaceholder("Select Country").pressSequentially("Ind")
+    await page.getByRole("button", {name:"India"}).nth(1).click() // select country from dropdown
+
+    await page.getByText("Place Order").click() // click on place order button
+
+    await expect(page.getByText(" Thankyou for the order. ")).toBeVisible() // verify order is placed successfully
 })
