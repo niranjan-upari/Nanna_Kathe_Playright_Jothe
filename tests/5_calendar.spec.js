8f5b2bc080befd3034ece17522356d8bd3483b85
niranjan <niranjan.upari@gmail.com>
2026-05-10 16:05:28 +0530

    Assertion for calendar

--- tests/5_calendar.spec.js
+++ tests/5_calendar.spec.js
@@ -5,6 +5,7 @@ test('Calendar test', async({page})=>{
     const monthNumber = "6"
     const date = "15"
     const year = "2027"
+    const expectedList =[monthNumber, date, year]
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
     await page.locator(".react-date-picker__inputGroup").click()
@@ -14,4 +15,12 @@ test('Calendar test', async({page})=>{
     await page.getByText(year).click()
     await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click()
     await page.locator("//abbr[text()='"+date+"']").click()
+
+    //assertion
+    const inputs = page.locator(".react-date-picker__inputGroup__input")
+
+    for (let i=0; i<expectedList.length; i++){
+        const value = await inputs.nth(i).inputValue()
+        expect(value).toBe(expectedList[i])
+    }
 })
