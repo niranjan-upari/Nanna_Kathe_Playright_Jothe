0bfba9949e16742f4b85b8d64d64f3d8a4f91a03
niranjan <niranjan.upari@gmail.com>
2026-05-10 08:32:42 +0530

    Calendar example

---
+++ tests/5_calendar.spec.js
@@ -0,0 +1,17 @@
+const{test,expect}= require('@playwright/test')
+
+test('Calendar test', async({page})=>{
+
+    const monthNumber = "6"
+    const date = "15"
+    const year = "2027"
+
+    await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+    await page.locator(".react-date-picker__inputGroup").click()
+    await page.locator(".react-calendar__navigation__label").click()
+    await page.locator(".react-calendar__navigation__label").click()
+
+    await page.getByText(year).click()
+    await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click()
+    await page.locator("//abbr[text()='"+date+"']").click()
+})
